0ffbd7440daaf7a92b37070b8a60b3e17b960e4d
shindeanant <shindeanant1997@gmail.com>
2021-10-08 14:28:56 +0530

    ADD: Contact Deatils Added

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,7 @@
 package com.bridgelabzz.AddressBookk;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -7,6 +9,21 @@ public class AddressBook {
 	private static final Logger LOG = LogManager.getLogger("App.class");
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book problem");
+		LOG.info("Wecome to address Book Program");
+		Scanner value = new Scanner(System.in);
+		int PhoneNumber;
+		String Address, FirstName, lastName;
+		LOG.info("Enter the first name");
+		FirstName = value.nextLine();
+		LOG.info("Enter the last name");
+		lastName = value.nextLine();
+		LOG.info("Enter the Address name");
+		Address = value.nextLine();
+		LOG.info("Enter the phone number");
+		PhoneNumber = value.nextInt();
+		LOG.info("FistName" + FirstName);
+		LOG.info("LastName" + lastName);
+		LOG.info("Address" + Address);
+		LOG.info("PhoneNumber" + PhoneNumber);
 	}
 }
